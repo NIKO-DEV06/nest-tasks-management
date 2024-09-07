2306153e091aedc07e430e56f475adaac9764c02
NIKO-DEV06 <ayenikoemmanuel06@gmail.com>
2024-09-07 16:37:25 +0100

    implemendted unit tests

---
+++ src/tasks/tasks.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test } from '@nestjs/testing';
+import { TasksService } from './tasks.service';
+import { TasksRepository } from './tasks.repository';
+
+const mockTasksRepository = () => ({
+  getTasks: jest.fn(),
+});
+
+const mockUser = {
+  username: 'Emma',
+  id: 'someid',
+  password: 'somepasssword',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let tasksService: TasksService;
+  let tasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        TasksService,
+        { provide: TasksRepository, useFactory: mockTasksRepository },
+      ],
+    }).compile();
+
+    tasksService = module.get(TasksService);
+    tasksRepository = module.get(TasksRepository);
+  });
+  describe('getTasks', () => {
+    it('calls TaskRepository.getTasks and returns the result', async () => {
+      tasksRepository.getTasks.mockResolvedValue('someValue');
+      const result = await tasksService.getTasks(null, mockUser);
+      expect(result).toEqual('someValue');
+    });
+  });
+});
